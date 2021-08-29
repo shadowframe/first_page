237ddda175e13bf2603c12bdf8e172c8f957acfb
Jan <jan.weber123@googlemail.com>
2021-08-29 22:59:33 +0200

    test2

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,7 +4,7 @@ import Layout from '../components/layout'
 
 const BlogPage = ({ data }) => {
     return (
-        <Layout pageTitle="My Blog Posts">
+        <Layout pageTitle="My Blog Posts :)">
         <ul>
       {
         data.allFile.nodes.map(node => (
