c4954d3e22e0c4584011fb136f966322f2ed123d
Jan <jan.weber123@googlemail.com>
2021-08-31 09:36:34 +0200

    erster test mit gitignore und gelöschter datei

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,6 +5,7 @@ const AboutPage = () => {
   return (
     <Layout pageTitle="About Me">
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+      <p>Some more text</p>
     </Layout>
   )
 }
